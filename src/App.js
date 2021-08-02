9cc16d3b00ab3d3f1df4922cd35a5dcad675b8b8
SaberGit <skalshetty57@gmail.com>
2021-08-03 01:03:20 +0530

    making a masters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages'
 import SigninPage from './pages/signin';
 
